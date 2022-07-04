28e6c16d55db6a89b0fd6a4f6ff35b47ebe8eccc
sumanraj <sumanrajkandel1@gmail.com>
2022-07-04 21:17:05 +0530

    {Initial Project setup}

---
+++ DemoAPIPostgres.Data/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAPIPostgres.Data
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Summery { get; set; }
+
+        public List<Actor> Actors { get; set; }
+    }
+
+    public class Actor
+    {
+        public int Id { get; set; }
+        [Required]
+        public string FullName { get; set; }
+    }
+}
---
+++ DemoAPIPostgres.Data/MovieDataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAPIPostgres.Data
+{
+    public class MovieDataContext : DbContext
+    {
+
+        public MovieDataContext(DbContextOptions<MovieDataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseSerialColumns();
+        }
+
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+    }
+}
